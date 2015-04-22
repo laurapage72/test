a86a5d6139bdfc7c72737c412cd3538ec98353bf
Laura Page <laurapage72@outlook.com>
2015-04-22 11:36:07 -0600

    Additional features to feature2

--- index.php
+++ index.php
@@ -2,4 +2,8 @@ this is another file
 <?php 
 echo"this is just a test, so I can test the commmit";
 ?>
+<<<<<<< Updated upstream
 this is an update to index.php
+=======
+more feature2 changes
+>>>>>>> Stashed changes
