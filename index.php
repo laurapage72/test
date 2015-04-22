f741daab6e679b688885e6ab28918ac9b84e93f6
Laura Page <laurapage72@outlook.com>
2015-04-22 11:31:37 -0600

    updating index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 this is another file
+<<<<<<< Updated upstream
 fixing bug*1243
+=======
+<?php 
+echo"this is just a test, so I can test the commmit";
+?>
+more feature2 changes
+>>>>>>> Stashed changes
